0a4c2602e0a38be9aa5766f86e10ab79800361a8
faust93 <monumentum@gmail.com>
2016-12-18 14:41:34 +0300

    implementing charging led & customization

--- liblight/lights.c
+++ liblight/lights.c
@@ -39,6 +39,12 @@
 #define MODE_DEFAULT  0x00
 #define MODE_BREATH   0x01
 #define MODE_BLINK    0x02
+#define MODE_MY       0x04
+
+#define L_NOTIFICATION  0x01
+#define L_BATTERY       0x02
+#define L_ATTENTION     0x03
+#define L_NONE          0xFF //255
 
 static pthread_once_t g_init = PTHREAD_ONCE_INIT;
 static pthread_mutex_t g_lock = PTHREAD_MUTEX_INITIALIZER;
@@ -52,9 +58,14 @@ char const*const LED_FILE
 char const*const LED_MODE_FILE
         = "/sys/devices/gpioi2c0.14/i2c-12/12-0030/led_pattern";
 
+char const*const LED_MY_PATTERN_FILE
+        = "/sys/devices/gpioi2c0.14/i2c-12/12-0030/my_pattern";
+
 char const*const LCD_FILE
         = "/sys/devices/virtual/backlight/pwm-backlight.0/brightness";
 
+char const*const BATTERY_STATUS
+        = "/sys/class/power_supply/bq2753x-0/status";
 /**
  * device methods
  */
@@ -63,6 +74,28 @@ void init_globals(void)
 {
     // init the mutex
     pthread_mutex_init(&g_lock, NULL);
+}
+
+static int
+write_str(char const* path, char *value)
+{
+    int fd;
+    static int already_warned = 0;
+
+    fd = open(path, O_RDWR);
+    if (fd >= 0) {
+        char buffer[64];
+        int bytes = sprintf(buffer, "%s\n", value);
+        int amt = write(fd, buffer, bytes);
+        close(fd);
+        return amt == -1 ? -errno : 0;
+    } else {
+        if (already_warned == 0) {
+            ALOGD("write_str failed to open %s\n", path);
+            already_warned = 1;
+        }
+        return -errno;
+    }
 }
 
 static int
@@ -130,7 +163,8 @@ set_light_locked(struct light_state_t const* state, int type)
 {
     int blink, mode = 0;
     int onMS, offMS;
-    unsigned int colorRGB;
+    char buffer[50];
+    unsigned int colorRGB, brightness;
 
     switch (state->flashMode) {
         case LIGHT_FLASH_TIMED:
@@ -145,12 +179,32 @@ set_light_locked(struct light_state_t const* state, int type)
             break;
     }
 
+    brightness = rgb_to_brightness(state);
+
+    if(type == L_BATTERY && brightness != 0){  //battery charging
+
+        char charging_status[15];
+        FILE* fp = fopen(BATTERY_STATUS, "rb");
+        fgets(charging_status, 14, fp);
+        fclose(fp);
+
+        if (strstr(charging_status, "Charging") != NULL) {
+                onMS = 2500;
+                offMS = 2500;
+            } else {
+                onMS = 0;
+            }
+    }
+
     colorRGB = state->color;
 
-
-    if (onMS > 0 && offMS > 0) {
+    if (onMS > 0) {
         blink = 1;
-        mode = MODE_BREATH;
+        mode = MODE_MY;
+        if(onMS!=1){
+            onMS = onMS / 60;
+            offMS = offMS / 60;
+        }
     } else {
         blink = 0;
         mode = MODE_DEFAULT;
@@ -160,6 +214,12 @@ set_light_locked(struct light_state_t const* state, int type)
     ALOGD("set_speaker_light_locked mode %d, colorRGB=%08X, onMS=%d, offMS=%d mode:%d\n",
             state->flashMode, colorRGB, onMS, offMS, mode);
 #endif
+    if(onMS == 1)
+        snprintf(buffer, sizeof(buffer), "2,0x01,0x7f,");
+    else
+        snprintf(buffer, sizeof(buffer), "8,0x%x,0x7f,0x60,0x00,0x%x,0xff,0x60,0x00,", onMS, offMS);
+
+    write_str(LED_MY_PATTERN_FILE, buffer);
     write_int(LED_MODE_FILE, mode);
 
     return 0;
@@ -170,7 +230,7 @@ set_light_notifications(struct light_device_t* dev,
         struct light_state_t const* state)
 {
     pthread_mutex_lock(&g_lock);
-    set_light_locked(state, 0);
+    set_light_locked(state, L_NOTIFICATION);
     pthread_mutex_unlock(&g_lock);
     return 0;
 }
@@ -182,7 +242,7 @@ set_light_attention(struct light_device_t* dev,
     ALOGD("light_attention mode %d,onMS=%d, offMS=%d\n",state->flashMode,state->flashOnMS,state->flashOffMS);
 
     pthread_mutex_lock(&g_lock);
-    set_light_locked(state, 1);
+    set_light_locked(state, L_ATTENTION);
     pthread_mutex_unlock(&g_lock);
     return 0;
 }
@@ -195,11 +255,10 @@ static int set_light_battery(struct light_device_t* dev,
     struct light_state_t* finalState =
         (struct light_state_t*) malloc(sizeof(struct light_state_t));
 
+    ALOGD("set_light_battery");
+
     memcpy(finalState, state, sizeof(struct light_state_t));
-
-//        finalState->flashMode = LIGHT_FLASH_TIMED;
-
-//    set_light_locked(finalState, 2);
+    set_light_locked(finalState, L_BATTERY);
     pthread_mutex_unlock(&g_lock);
 
     free(finalState);
